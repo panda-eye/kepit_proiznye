2e4c53c2224122ea1a47441d5637d4fdf74ba99c
Vova Lantsov <29947632+panda-eye@users.noreply.github.com>
2018-01-07 18:22:40 +0200

    Added files

---
+++ KEPIT_Proiznye/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Проїзні")]
+[assembly: AssemblyDescription("Програма для ведення обліку заказів проїзних в Коледжі економіки, права та інформаційних технологій.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("КЕПІТ")]
+[assembly: AssemblyProduct("Проїзні")]
+[assembly: AssemblyCopyright("Вова Ланцов")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ea965fc4-c2a3-4fc3-bd70-a7eec6f49397")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
